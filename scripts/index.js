f58de7e30eed1fe0150c501bf746fd370aaa245e
RYAN MOORE <moorek11c@gmail.com>
2024-05-24 11:19:55 -0700

    6 array

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,6 @@ const initialCards = [
   },
   {
     title: "Bald Mountains",
-
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
   {
